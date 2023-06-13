272b6789552ce168bdcee83e3b5af99a351d2fe7
iwanharyatno <iwnharry61@gmail.com>
2023-06-13 08:19:27 +0700

    Add message limit

--- src/components/ChatPool.jsx
+++ src/components/ChatPool.jsx
@@ -1,7 +1,16 @@
+import { useRef, useEffect } from 'react';
+
 function ChatPool({ messages, className }) {
+  const containerRef = useRef(null);
+  const maxMessages = messages.slice(messages.length - 10, messages.length);
+
+  useEffect(() => {
+    containerRef.scrollTop = containerRef.scrollHeight;
+  }, [messages]);
+
   return (
-    <div className={'overflow-scroll bg-gray-100/50 ' + className}>
-      {messages && messages.map(message => (<ChatBox message={message} />))}
+    <div className={'overflow-scroll bg-gray-100/50 ' + className} ref={containerRef}>
+      {maxMessages && maxMessages.map(message => (<ChatBox message={message} />))}
     </div>
   );
 }
