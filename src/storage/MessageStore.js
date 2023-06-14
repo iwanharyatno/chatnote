20f42504ab22a99d50af6b88dab3b8b7bc50987b
iwanharyatno <iwnharry61@gmail.com>
2023-06-14 08:49:49 +0700

    Add timezone offset to message's dates after retrieving from localStorage

--- src/storage/MessageStore.js
+++ src/storage/MessageStore.js
@@ -1,5 +1,8 @@
 const APP_MESSAGE_STORE_KEY = "messages";
 
+const SECONDS_IN_MINUTE = 60;
+const MILLIS_IN_SECOND = 1000;
+
 const MessageStore = {
   save: function(messages) {
     localStorage.setItem(APP_MESSAGE_STORE_KEY, JSON.stringify(messages));
@@ -7,11 +10,15 @@ const MessageStore = {
   getAll: function() {
     const messagesJson = localStorage.getItem(APP_MESSAGE_STORE_KEY);
     if (messagesJson) {
-      return JSON.parse(messagesJson).map(message => ({
-        id: message.id,
-        date: new Date(message.date),
-        content: message.content,
-      }));
+      return JSON.parse(messagesJson).map(message => {
+        const d = new Date(message.date);
+        const timeOffsetMillis = d.getTimezoneOffset() * SECONDS_IN_MINUTE * MILLIS_IN_SECOND;
+        return {
+          id: message.id,
+          date: new Date(d.getTime() - timeOffsetMillis),
+          content: message.content,
+        };
+      });
     }
     return [];
   }
